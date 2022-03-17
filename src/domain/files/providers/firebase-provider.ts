11b83b30204598e79e8e06f92dde4849022ebefd
Abner Persio <binhopersio@gmail.com>
2022-03-16 23:58:28 -0300

    refactor: firebase provider

--- src/domain/files/providers/firebase-provider.ts
+++ src/domain/files/providers/firebase-provider.ts
@@ -2,20 +2,19 @@ import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 import { firebaseStorage } from '@config/firebase';
 
+import { SendFileParams } from '../types/file';
 import { FileProvider } from './file-provider';
 
 export class FirebaseProvider extends FileProvider {
   static readonly clientName = 'firebase';
 
-  async upload(file: Express.Multer.File, metadata: Record<string, any>): Promise<string | null> {
-    const fileName = this.formatFileName(file.originalname);
+  async sendFile({ file, fileName, metadata }: SendFileParams): Promise<string | null> {
     const FILE_PATH = `uploads/${fileName}`;
 
     const fileStorageRef = ref(firebaseStorage, FILE_PATH);
 
     await uploadBytes(fileStorageRef, file.buffer, metadata);
-    const pathLocation = await getDownloadURL(fileStorageRef);
 
-    return pathLocation;
+    return await getDownloadURL(fileStorageRef);
   }
 }
