54c383b8de793c4a3442f1f1ed0fa1283e24abbe
Abner Persio <binhopersio@gmail.com>
2022-04-05 19:04:16 -0300

    feat: adds esbuild

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -1,11 +1,3 @@
 import { config } from 'dotenv';
-import { join, resolve } from 'path';
 
-const projectFolder = resolve(__dirname, '..', '..', '..');
-
-config({
-  path:
-    process.env.NODE_ENV === 'test'
-      ? join(projectFolder, '.env.test')
-      : join(projectFolder, '.env'),
-});
+config();
